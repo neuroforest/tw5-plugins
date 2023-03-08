85e4b6c50eb3e1f2c8aacd263e08122568697524
Urban Alich <urbanalich@gmail.com>
2023-03-08 22:38:01 +0100

    ADD: server route GET /neuro/info

---
+++ source/core/server/routes/get-info.js
@@ -0,0 +1,24 @@
+/*\
+title: $:/plugins/neuroforest/core/server/routes/get-info.js
+module-type: route
+type: application/javascript
+GET /neuro/info
+\*/
+(function() {
+
+"use strict";
+
+exports.method = "GET";
+
+exports.path = /^\/neuro\/info$/;
+
+exports.handler = function(request,response,state) {
+  var info = {
+    "local-path": $tw.boot.wikiTiddlersPath
+  }
+  // Write response
+  response.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
+  response.end(JSON.stringify(info),"utf8");
+};
+
+}());
